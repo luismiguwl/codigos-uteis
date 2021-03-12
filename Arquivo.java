c79b43b185f23122c2efaf2a9b11f7c6f429c479
Luis Miguel Souza <68262792+luismiguwl@users.noreply.github.com>
2021-03-12 14:29:53 -0300

    Add files via upload

---
+++ Arquivo.java
@@ -0,0 +1,32 @@
+package com.luis.arquivo;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Arquivo {
+    
+    private static String destino = "Filmes assistidos em 2021.csv";
+
+    public static List<String> lerArquivo() {
+        List<String> dados = new ArrayList<>();
+
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(destino), "UTF-8"));
+            
+            String linha = reader.readLine();
+            
+            while (linha != null) {
+                dados.add(linha);
+                linha = reader.readLine();
+            }
+            
+            reader.close();
+        } catch (Exception e) {
+        }
+        
+        return dados;
+    }
+}
