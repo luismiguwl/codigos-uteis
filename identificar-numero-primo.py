f7ff1d50f95321066a0eadf66f8a57b09048997c
Luis Miguel <luisbwrb@gmail.com>
2021-04-01 18:27:17 -0300

    uniao de ifs para fins de limpeza de classe

--- identificar-numero-primo.py
+++ identificar-numero-primo.py
@@ -2,16 +2,10 @@ def identificarNumeroPrimo(numero):
 	if numero == 1:
 		return False
 
-	if numero == 2:
+	if numero == 2 or numero == 3:
 		return True
 
-	if numero == 3:
-		return True
-
-	if numero % 2 == 0:
-		return False
-
-	if numero % 3 == 0:
+	if numero % 2 == 0 or numero % 3 == 0:
 		return False
 
 	return True
